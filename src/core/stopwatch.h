b6013d8c7e59d7e1bf32de6b75d2b765f1b2a95b
Benjamin A. Himes <himes.benjamin@gmail.com>
2021-07-21 17:16:54 -0400

    Timer is mostly working, just need to add the gating for sub methods, included now a thread safe to check on thread id (which is set to zero if no openmp) also set flags in main part of unblur2. Found hot spots in the electron dose calculation, test at scale after completeing the timing method.

--- src/core/stopwatch.h
+++ src/core/stopwatch.h
@@ -17,10 +17,10 @@ public:
 	virtual ~StopWatch();
 
 	// dummy methods
-	inline void start(std::string name) {return;}
-	inline void lap(std::string name) {return;}
-	inline void check_for_name_and_set_current_idx(std::string name) {return;}
-	inline void mark_entry_or_exit_point() {return;}
+	inline void start(std::string name, bool thread_safe = true) {return;}
+	inline void lap(std::string name, bool thread_safe = true) {return;}
+	inline void print_times() {return;}
+	inline void mark_entry_or_exit_point( bool thread_safe = true) {return;}
 
 };
 
@@ -36,14 +36,15 @@ public:
 	virtual ~StopWatch();
 
 
+
 	// Create or reuse event named "name", start timing. May overlap with other timing events.
-	void start(std::string name);
+	void start(std::string name, bool thread_safe = true);
 
 	// Record the elapsed time since last "start" for this event. Add to cummulative time.
-	void lap(std::string name);
+	void lap(std::string name, bool thread_safe = true);
 
 	// Print out all event times, including stopwatch overhead time.
-	void print_times();
+	void print_times(bool  thread_safe = true);
 
 	// Start or pause the total elapsed time when passing a stopwatch pointer to a method. Place inside the method at the entry and exit point of the method call.
 	void mark_entry_or_exit_point();
@@ -56,9 +57,11 @@ public:
 private:
 
 	enum TimeFormat : int { NANOSECONDS, MICROSECONDS, MILLISECONDS, SECONDS };
+	enum SpecialIDX : int { total_elapsed = 0, total_measured = 1};
+	
 	typedef std::chrono::time_point<std::chrono::high_resolution_clock> time_pt;
 
-
+	int number_of_events_being_recorded;
 	std::vector<std::string> event_names = {};
 	std::vector<time_pt> 	 event_times = {};
 	std::vector<uint64_t> 	 elapsed_times = {};
@@ -68,6 +71,8 @@ private:
 	uint64_t null_time;
 	bool is_new;
 	bool is_set_overall;
+	bool is_recording_measured_time;
+	bool is_recording_elapsed_time;
 	TimeFormat time_fmt = MICROSECONDS;
 
 	uint64_t stop(TimeFormat T, int idx);
@@ -78,6 +83,11 @@ private:
 
 		// Check to see if an event named "name" already exists, if not, initialize it.
 	void check_for_name_and_set_current_idx(std::string name);
+
+	void record_start(std::string name);
+	void record_end();
+	void record_elapsed();
+	void record_measured();
 
 };
 } // namespace cistem_timer
